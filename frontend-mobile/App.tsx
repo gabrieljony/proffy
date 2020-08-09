015c2fbede3b7b524cc2ee5a32d9e746e1192a5d
gabriel.borges.silva <gabriel.borges.silva@avanade.com>
2020-08-09 16:30:19 -0300

    Finalizado parte mobile do NLW2

--- frontend-mobile/App.tsx
+++ frontend-mobile/App.tsx
@@ -1,21 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { AppLoading } from "expo";
+import AppStack from "./src/routes/AppStack";
+
+import {
+  Ubuntu_400Regular,
+  Ubuntu_700Bold,
+  useFonts,
+} from "@expo-google-fonts/ubuntu";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  let [fontsLoaded] = useFonts({
+    Ubuntu_400Regular,
+    Ubuntu_700Bold,
+  });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <>
+        <AppStack />
+        <StatusBar style="light" />
+      </>
+    );
+  }
+}
